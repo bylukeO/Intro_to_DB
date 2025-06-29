87360cb9e01a240db2deffd75e57fac977e03869
Luke Oladejo <bylukeo@gmail.com>
2025-06-29 13:05:45 +0100

    update task2.sql file

--- task_2.sql
+++ task_2.sql
@@ -39,3 +39,5 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     book_id INT NOT NULL,
     quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
